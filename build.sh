a6cda274095d4ed6dd6e9ecbe6fb0612afbd2b2f
zhenpeng ge <zhenpeng.ge@qq.com>
2021-10-08 20:54:09 +0800

    update build script

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# usage: build.sh 20.04-cu11.0
+# usage: ./build_and_push.sh 20.04-cu11.0
 
 # echo "argv: $1"
 UBUNTU_VERSION=`echo $1 | awk -F '-cu' '{print $1}'`
 CUDA_VERSION=`echo $1 | awk -F '-cu' '{print $2}'`
+echo "ubuntu version:${UBUNTU_VERSION},cuda version:${CUDA_VERSION}"
 
 # check ubuntu version
 if [[(${UBUNTU_VERSION} != "18.04") && (${UBUNTU_VERSION} != "20.04")]];then
--- build_and_push.sh
+++ build_and_push.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# usage: build_and_push.sh 20.04-cu11.0
+# usage: ./build_and_push.sh 20.04-cu11.0
 
-# change v20.04-cu11.0 to 20.04-cu11.0
-arg=${1//v}
-UBUNTU_VERSION=`echo $arg | awk -F '-cu' '{print $1}'`
-CUDA_VERSION=`echo $arg | awk -F '-cu' '{print $2}'`
-echo "${UBUNTU_VERSION}, ${CUDA_VERSION}"
+# echo "argv: $1"
+UBUNTU_VERSION=`echo $1 | awk -F '-cu' '{print $1}'`
+CUDA_VERSION=`echo $1 | awk -F '-cu' '{print $2}'`
+echo "ubuntu version:${UBUNTU_VERSION},cuda version:${CUDA_VERSION}"
 
 # check ubuntu version
 if [[(${UBUNTU_VERSION} != "18.04") && (${UBUNTU_VERSION} != "20.04")]];then
@@ -23,7 +22,7 @@ if [[ $? != 0 ]]; then
 fi
 
 # #build ubuntu-desktop image
-DOCKER_TAG = ${UBUNTU_VERSION}-cu${CUDA_VERSION}
+DOCKER_TAG=${UBUNTU_VERSION}-cu${CUDA_VERSION}
 docker build ubuntu-desktop/${UBUNTU_VERSION} --file ubuntu-desktop/${UBUNTU_VERSION}/Dockerfile  \
              --build-arg CUDAGL_TAG=${CUDAGL_TAG} \
              --tag ubuntu-desktop:${DOCKER_TAG}
@@ -31,6 +30,7 @@ if [[ $? != 0 ]]; then
     echo "Failed to build docker image 'ubuntu-desktop:${DOCKER_TAG}'"
     exit -3
 fi
+
 # push to docker hub
 docker tag ubuntu-desktop:${DOCKER_TAG} gezp/ubuntu-desktop:${DOCKER_TAG}
 docker push gezp/ubuntu-desktop:${DOCKER_TAG}
