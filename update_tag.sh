84799026acf762bbd97e3ba5cf77e57fc006e5b7
zhenpeng ge <zhenpeng.ge@qq.com>
2021-10-09 11:50:14 +0800

    update

---
+++ update_tag.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ubuntu18_cuda_tags=(10.1 10.2 11.0 11.1 11.2.0 11.3.0 11.4.0)
+ubuntu20_cuda_tags=(11.0 11.1 11.2.0 11.3.0 11.4.0)
+
+for value in ${ubuntu18_cuda_tags[@]}
+do
+    git tag 18.04-cu$value -f
+    git push origin 18.04-cu$value -f
+done
+
+for value in ${ubuntu20_cuda_tags[@]}
+do
+    git tag 20.04-cu$value -f
+    git push origin 20.04-cu$value -f
+done
+
+exit 0
