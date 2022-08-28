72e8f84fe77f3ce025ef3265d8cc1928edfcd5fc
zhenpeng ge <zhenpeng.ge@qq.com>
2022-08-28 14:54:36 +0800

    update helper script

--- docker_push.sh
+++ docker_push.sh
@@ -2,23 +2,7 @@
 
 # usage: ./docker_push.sh 20.04-cu11.0
 
-# echo "argv: $1"
-UBUNTU_VERSION=`echo $1 | awk -F '-cu' '{print $1}'`
-CUDA_VERSION=`echo $1 | awk -F '-cu' '{print $2}'`
-echo "ubuntu version:${UBUNTU_VERSION},cuda version:${CUDA_VERSION}"
-
-# check ubuntu version
-if [[(${UBUNTU_VERSION} != "18.04") && (${UBUNTU_VERSION} != "20.04") && (${UBUNTU_VERSION} != "22.04")]];then
-    echo "Invalid ubuntu version:${UBUNTU_VERSION}"
-    exit -1
-fi
-
-# #build ubuntu-desktop image
-if [[("${CUDA_VERSION}" == "")]];then
-    DOCKER_TAG=${UBUNTU_VERSION}
-else
-    DOCKER_TAG=${UBUNTU_VERSION}-cu${CUDA_VERSION}
-fi
+DOCKER_TAG=$1
 
 # push to docker hub
 docker tag ubuntu-desktop:${DOCKER_TAG} gezp/ubuntu-desktop:${DOCKER_TAG}
