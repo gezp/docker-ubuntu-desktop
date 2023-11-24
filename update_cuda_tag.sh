dc461e7925b3917dcec53e04e3b033c501ccae96
zhenpeng ge <zhenpeng.ge@qq.com>
2023-11-24 14:22:14 +0000

    update ci for cuda

--- update_cuda_tag.sh
+++ update_cuda_tag.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-ubuntu_tags=(18.04 20.04 22.04)
 ubuntu18_cuda_tags=(11.0.3 11.1.1 11.2.2)
 ubuntu20_cuda_tags=(11.0.3 11.1.1 11.2.2 11.3.1 11.4.3 11.5.2 11.6.2 11.7.1)
 ubuntu22_cuda_tags=(11.7.1 11.8.0 12.0.1 12.1.1)
-
-for value in ${ubuntu_tags[@]}
-do
-    git tag $value -f
-    git push origin $value -f
-done
 
 for value in ${ubuntu18_cuda_tags[@]}
 do
