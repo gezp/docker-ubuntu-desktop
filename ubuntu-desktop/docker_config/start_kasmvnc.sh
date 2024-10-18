57b1027ef6c4f497184aaf6ca48a994c6b3e7621
Zhenpeng Ge <zhenpeng.ge@qq.com>
2024-10-19 01:05:18 +0800

    rename environment variables

--- ubuntu-desktop/docker_config/start_kasmvnc.sh
+++ ubuntu-desktop/docker_config/start_kasmvnc.sh
@@ -5,6 +5,6 @@ if [ ! -f "/home/$USER/.vnc/passwd" ]; then
 fi
 rm -rf /tmp/.X1000-lock /tmp/.X11-unix/X1000
 # start kasmvnc
-su $USER -c "kasmvncserver :1000 -select-de xfce -interface 0.0.0.0 -websocketPort 4000 -cert $SERVER_SSL_CERT -key $SERVER_SSL_KEY -RectThreads $VNC_THREADS"
+su $USER -c "kasmvncserver :1000 -select-de xfce -interface 0.0.0.0 -websocketPort 4000 -cert $HTTPS_CERT -key $HTTPS_CERT_KEY -RectThreads $VNC_THREADS"
 su $USER -c "pulseaudio --start"
 tail -f /home/$USER/.vnc/*.log
--- ubuntu-desktop/docker_config/start_novnc.sh
+++ ubuntu-desktop/docker_config/start_novnc.sh
@@ -7,5 +7,5 @@ rm -rf /tmp/.X1000-lock /tmp/.X11-unix/X1000
 # start TurboVNC
 su $USER -c "/opt/TurboVNC/bin/vncserver :1000 -rfbport 5900"
 # start NoVNC
-su $USER -c "/opt/noVNC/utils/novnc_proxy --vnc localhost:5900 --ssl-only --cert $SERVER_SSL_CERT --key $SERVER_SSL_KEY --listen 4000 --heartbeat 10 &"
+su $USER -c "/opt/noVNC/utils/novnc_proxy --vnc localhost:5900 --ssl-only --cert $HTTPS_CERT --key $HTTPS_CERT_KEY --listen 4000 --heartbeat 10 &"
 tail -f /home/$USER/.vnc/*.log
