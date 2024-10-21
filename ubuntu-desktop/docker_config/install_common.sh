73da38a7ff35b81255aed5efda9b435e533a8515
Zhenpeng Ge <zhenpeng.ge@qq.com>
2024-10-21 22:57:35 +0800

    update common tools

--- ubuntu-desktop/docker_config/install_common.sh
+++ ubuntu-desktop/docker_config/install_common.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 apt-get update
-apt-get install -y sudo vim locales gnupg2 wget curl lsb-release bash-completion
-apt-get install -y net-tools iputils-ping mesa-utils software-properties-common openssh-server
+apt-get install -y sudo vim gedit locales gnupg2 wget curl zip lsb-release bash-completion
+apt-get install -y net-tools iputils-ping mesa-utils software-properties-common build-essential
 apt-get install -y python3 python3-pip python3-numpy
-apt-get install -y git git-lfs tmux 
+apt-get install -y openssh-server git git-lfs tmux
