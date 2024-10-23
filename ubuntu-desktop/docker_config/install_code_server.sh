ea161d0084a7b9a5946ee208a3e10650b2cd59aa
Zhenpeng Ge <zhenpeng.ge@qq.com>
2024-10-24 07:57:35 +0800

    fix code server

--- ubuntu-desktop/docker_config/install_code_server.sh
+++ ubuntu-desktop/docker_config/install_code_server.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 version=4.93.1
 arch=$(dpkg --print-architecture)
+codename=$(lsb_release --short --codename)
+if [[ $codename == 'bionic' ]]; then
+    version=4.16.1
+fi
 curl -fSL "https://github.com/coder/code-server/releases/download/v${version}/code-server_${version}_${arch}.deb" -o code-server.deb
 dpkg -i ./code-server.deb
 rm code-server.deb
