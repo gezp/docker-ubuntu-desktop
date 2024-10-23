996a98f9e90dde65438c07477bcd221fbf2b4491
Zhenpeng Ge <zhenpeng.ge@qq.com>
2024-10-24 07:52:39 +0800

    fix kasmvnc

--- ubuntu-desktop/docker_config/install_kasmvnc.sh
+++ ubuntu-desktop/docker_config/install_kasmvnc.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 version=1.3.2
+arch=$(dpkg --print-architecture)
+codename=$(lsb_release --short --codename)
 if [[ $codename == 'bionic' ]]; then
     version=1.3.1
 fi
-arch=$(dpkg --print-architecture)
-codename=$(lsb_release --short --codename)
 curl -fSL "https://github.com/kasmtech/KasmVNC/releases/download/v${version}/kasmvncserver_${codename}_${version}_$arch.deb" -o kasmvncserver.deb
 apt-get install -y kasmvncserver.deb
 rm kasmvncserver.deb
