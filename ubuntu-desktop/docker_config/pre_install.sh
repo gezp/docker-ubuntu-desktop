aae1692e5384e97484f0247ccf078670cd0c6d4f
Zhenpeng Ge <zhenpeng.ge@qq.com>
2024-10-24 09:00:42 +0800

    update pre install

--- ubuntu-desktop/docker_config/pre_install.sh
+++ ubuntu-desktop/docker_config/pre_install.sh
@@ -3,4 +3,4 @@ apt-get update
 apt-get install -y sudo vim gedit locales gnupg2 wget curl zip lsb-release bash-completion
 apt-get install -y net-tools iputils-ping mesa-utils software-properties-common build-essential
 apt-get install -y python3 python3-pip python3-numpy
-apt-get install -y openssh-server git git-lfs tmux
+apt-get install -y openssh-server openssl git git-lfs tmux
